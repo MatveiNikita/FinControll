e6ebea05b36b12fe34f22cc829b7f7e67c78c986
NIKITA_MATVEI <matveinikitaivanovich@gmail.com>
2025-03-31 12:15:24 +0300

    Started to create App FinControl

--- src/main/java/md/crudproject/fincontrol/models/User.java
+++ src/main/java/md/crudproject/fincontrol/models/User.java
@@ -1,17 +1,25 @@
 package md.crudproject.fincontrol.models;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import java.time.LocalDate;
 import java.util.UUID;
 
+
 @Entity
-@Table(name = "User")
-@Getter
-@Setter
-@RequiredArgsConstructor
-@ToString
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -22,8 +30,15 @@ public class User {
 
     private String surname;
 
+    private LocalDate dateOfBirth;
+
     @Column(unique = true, nullable = false)
     private String email;
+    private String password;
 
-    private LocalDate dateOfBirth;
+    public void setPassword(String password){
+        this.password = PASSWORD_ENCODE.encode(password);
+    }
+
+    private static final PasswordEncoder PASSWORD_ENCODE = new BCryptPasswordEncoder();
 }
--- src/main/resources/db/migration/V1__CreateTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE User
-(
-    ID uuid UNIQUE PRIMARY KEY ,
-    name CHARACTER(64),
-    surname CHARACTER(64),
-    email CHARACTER(128) UNIQUE,
-    data DATE
-)
